92871fa18318ed28e5da7fee5be30be7ed0d48ea
ViktoriaWofC <vik-tori-s@yandex.ru>
2016-12-03 18:58:40 +0400

    end (maybe)

--- app/src/main/java/com/user/example/regressionapp/MainActivity.java
+++ app/src/main/java/com/user/example/regressionapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.user.example.regressionapp;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,9 +11,16 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.model.XYSeries;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -30,10 +38,26 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tv;
     private Button bt1;
     private Button bt2;
     private Context context;
+    RegressAsyncTask regressAsyncTask;
+    double[] values1;
+    double[] values2;
+    double[] values3;
+    String val1;
+    String val2;
+    String val3;
+
+    private GraphicalView mChart;
+    private XYMultipleSeriesDataset mDataset = new XYMultipleSeriesDataset();
+    private XYMultipleSeriesRenderer mRenderer = new XYMultipleSeriesRenderer();
+    private XYSeries mCurrentSeries1;
+    private XYSeries mCurrentSeries2;
+    private XYSeries mCurrentSeries3;
+    private XYSeriesRenderer mCurrentRenderer1;
+    private XYSeriesRenderer mCurrentRenderer2;
+    private XYSeriesRenderer mCurrentRenderer3;
 
     public static String TAG="TAAG";
 
@@ -48,40 +72,101 @@ public class MainActivity extends AppCompatActivity {
 
         context = MainActivity.this;
 
-        tv = (TextView)findViewById(R.id.text_test);
 
-        final MyAsyncTask t = new MyAsyncTask();
-
-
-        bt1 = (Button)findViewById(R.id.button_test1);
+        bt1 = (Button)findViewById(R.id.button_calc);
         bt1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                t.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+                regressAsyncTask = new RegressAsyncTask();
+                regressAsyncTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
             }
         });
 
-        bt2 = (Button)findViewById(R.id.button_test2);
+        bt2 = (Button)findViewById(R.id.button_show);
+        bt2.setEnabled(false);
         bt2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                t.cancel(false);
+                Log.d(TAG, "Start convert");
+                String[] str;
+                int n;
+
+                str = val1.split(",");
+                n = str.length;
+                values1 = new double[n];
+                for(int i = 0; i< n; i++)
+                    values1[i] = Double.valueOf(str[i]);
+
+                str = val2.split(",");
+                n = str.length;
+                values2 = new double[n];
+                for(int i = 0; i< n; i++)
+                    values2[i] = Double.valueOf(str[i]);
+
+                str = val3.split(",");
+                n = str.length;
+                values3 = new double[n];
+                for(int i = 0; i< n; i++)
+                    values3[i] = Double.valueOf(str[i]);
+
+                Log.d(TAG, "End convert");
+
+                ///////////////////////////////////////////////
+                LinearLayout layout = (LinearLayout) findViewById(R.id.linear);
+                if (mChart == null) {
+                    initChart();
+                    addSampleData();
+                    mRenderer.setXTitle("Наблюдения");
+                    mRenderer.setYTitle("Оценка");
+                    mChart = ChartFactory.getCubeLineChartView(MainActivity.this, mDataset, mRenderer, 0.3f);
+                    layout.addView(mChart);
+                } else {
+                    mChart.repaint();
+                }
+
             }
         });
 
     }
 
-    class MyAsyncTask extends AsyncTask<Object, Object, String> {
+    private void initChart() {
+        mCurrentSeries1 = new XYSeries("10 признаков");
+        mDataset.addSeries(mCurrentSeries1);
+        mCurrentSeries2 = new XYSeries("7 признаков");
+        mDataset.addSeries(mCurrentSeries2);
+        mCurrentSeries3 = new XYSeries("5 признаков");
+        mDataset.addSeries(mCurrentSeries3);
+
+        mCurrentRenderer1 = new XYSeriesRenderer();
+        mCurrentRenderer1.setColor(Color.BLUE);
+        mRenderer.addSeriesRenderer(mCurrentRenderer1);
+        mCurrentRenderer2 = new XYSeriesRenderer();
+        mCurrentRenderer2.setColor(Color.RED);
+        mRenderer.addSeriesRenderer(mCurrentRenderer2);
+        mCurrentRenderer3 = new XYSeriesRenderer();
+        mCurrentRenderer3.setColor(Color.MAGENTA);
+        mRenderer.addSeriesRenderer(mCurrentRenderer3);
+    }
+
+    private void addSampleData() {
+        for(int i = 0; i<values1.length; i++) {
+            mCurrentSeries1.add(i,values1[i]);
+            mCurrentSeries2.add(i,values2[i]);
+            mCurrentSeries3.add(i,values3[i]);
+        }
+    }
+
+    class RegressAsyncTask extends AsyncTask<Object, Object, String> {
 
         @Override
         protected String doInBackground(Object... voids) {
-            Log.d("!!!!!!!!!!!!!", "doInBackground");
+            Log.d("!!!", "Start Async");
             BufferedReader rd = null;
             StringBuilder sb = null;
             //String line = null;
             String s = "";
             URL url;
-            path = "http://10.60.1.212:4444/server/getValues";
+            path = "http://192.168.1.103:4444/server/getResult";
             Log.d(TAG, path);
             try {
                 url = new URL(path);
@@ -90,8 +175,7 @@ public class MainActivity extends AppCompatActivity {
                 connection.setConnectTimeout(15000);
                 connection.setDoInput(true);
                 connection.connect();
-                //InputStream in = connection.getInputStream();
-                //in.re
+
                 int status = connection.getResponseCode();
                 InputStream in;
                 if(status >= HttpURLConnection.HTTP_BAD_REQUEST)
@@ -99,7 +183,6 @@ public class MainActivity extends AppCompatActivity {
                 else
                     in = connection.getInputStream();
 
-                //rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                 rd = new BufferedReader(new InputStreamReader(in));
                 sb = new StringBuilder();
                 String line;
@@ -107,7 +190,6 @@ public class MainActivity extends AppCompatActivity {
                     sb.append(line);
                 }
                 Log.d(TAG, s);
-                //return sb.toString();
             } catch (MalformedURLException e) {
                 e.printStackTrace();
                 Log.d(TAG, "MalformedURLException");
@@ -119,7 +201,7 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
 
-            String str = "{\"friends\":"+sb.toString()+"}";
+            String str = "{\"result\":"+sb.toString()+"}";
 
             return str;
         }
@@ -135,25 +217,25 @@ public class MainActivity extends AppCompatActivity {
 
             try {
                 dataJsonObj = new JSONObject(s);
-                JSONArray friends = dataJsonObj.getJSONArray("friends");
+                JSONArray friends = dataJsonObj.getJSONArray("result");
 
-                // 1. достаем инфо о втором друге - индекс 1
-                //JSONObject secondFriend = friends.getJSONObject(1);
-                //secondName = secondFriend.getString("text");
-                //Log.d("TAAG", "Второе имя: " + secondName);
-
-                // 2. перебираем и выводим контакты каждого друга
                 for (int i = 0; i < friends.length(); i++) {
                     JSONObject friend = friends.getJSONObject(i);
 
-                    JSONObject contacts = friend.getJSONObject("Test");
+                    JSONObject contacts = friend.getJSONObject("Result");
 
-                    String phone = contacts.getString("id");
-                    String email = contacts.getString("text");
+                    String res = contacts.getString("result");
 
-                    Log.d(TAG, "phone: " + phone);
-                    Log.d(TAG, "email: " + email);
+                    Log.d(TAG, "result: " +i+" "+ res);
+
+                    res = res.substring(0,res.length()-1);
+                    res = res.substring(1);
+                    if(i==0)
+                        val1 = res;
+                    else if(i==1) val2 = res;
+                    else val3 = res;
                 }
+                bt2.setEnabled(true);
 
             } catch (JSONException e) {
                 e.printStackTrace();
@@ -162,8 +244,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
