36d36e7d577b320398c990c942b78bb645065a01
ViktoriaWofC <vik-tori-s@yandex.ru>
2016-12-10 17:56:39 +0400

    end all

--- app/src/main/java/com/user/example/regressionapp/MainActivity.java
+++ app/src/main/java/com/user/example/regressionapp/MainActivity.java
@@ -40,28 +40,45 @@ public class MainActivity extends AppCompatActivity {
 
     private Button bt1;
     private Button bt2;
+    private Button bt3;
     private Context context;
     RegressAsyncTask regressAsyncTask;
-    double[] values1;
-    double[] values2;
-    double[] values3;
-    String val1;
-    String val2;
-    String val3;
+    double[] values = new double[7];
+    double[] values10;
+    double[] values9;
+    double[] values8;
+    double[] values7;
+    double[] values6;
+    double[] values5;
+    double[] values4;
+    String val10;
+    String val9;
+    String val8;
+    String val7;
+    String val6;
+    String val5;
+    String val4;
+
 
     private GraphicalView mChart;
+    private GraphicalView mChart2;
     private XYMultipleSeriesDataset mDataset = new XYMultipleSeriesDataset();
     private XYMultipleSeriesRenderer mRenderer = new XYMultipleSeriesRenderer();
+    private XYMultipleSeriesDataset mDataset2 = new XYMultipleSeriesDataset();
+    private XYMultipleSeriesRenderer mRenderer2 = new XYMultipleSeriesRenderer();
     private XYSeries mCurrentSeries1;
     private XYSeries mCurrentSeries2;
     private XYSeries mCurrentSeries3;
+    private XYSeries currentSeries;
     private XYSeriesRenderer mCurrentRenderer1;
     private XYSeriesRenderer mCurrentRenderer2;
     private XYSeriesRenderer mCurrentRenderer3;
+    private XYSeriesRenderer currentRenderer;
 
     public static String TAG="TAAG";
 
     String path = "http://192.168.1.103:4444/server/getValues";
+    //10.60.0.171
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -91,29 +108,54 @@ public class MainActivity extends AppCompatActivity {
                 String[] str;
                 int n;
 
-                str = val1.split(",");
+                str = val10.split(",");
                 n = str.length;
-                values1 = new double[n];
+                values10 = new double[n];
                 for(int i = 0; i< n; i++)
-                    values1[i] = Double.valueOf(str[i]);
+                    values10[i] = Double.valueOf(str[i]);
 
-                str = val2.split(",");
+                str = val9.split(",");
                 n = str.length;
-                values2 = new double[n];
+                values9 = new double[n];
                 for(int i = 0; i< n; i++)
-                    values2[i] = Double.valueOf(str[i]);
+                    values9[i] = Double.valueOf(str[i]);
 
-                str = val3.split(",");
+                str = val8.split(",");
                 n = str.length;
-                values3 = new double[n];
+                values8 = new double[n];
                 for(int i = 0; i< n; i++)
-                    values3[i] = Double.valueOf(str[i]);
+                    values8[i] = Double.valueOf(str[i]);
+
+                str = val7.split(",");
+                n = str.length;
+                values7 = new double[n];
+                for(int i = 0; i< n; i++)
+                    values7[i] = Double.valueOf(str[i]);
+
+                str = val6.split(",");
+                n = str.length;
+                values6 = new double[n];
+                for(int i = 0; i< n; i++)
+                    values6[i] = Double.valueOf(str[i]);
+
+                str = val5.split(",");
+                n = str.length;
+                values5 = new double[n];
+                for(int i = 0; i< n; i++)
+                    values5[i] = Double.valueOf(str[i]);
+
+                str = val4.split(",");
+                n = str.length;
+                values4 = new double[n];
+                for(int i = 0; i< n; i++)
+                    values4[i] = Double.valueOf(str[i]);
 
                 Log.d(TAG, "End convert");
 
                 ///////////////////////////////////////////////
                 LinearLayout layout = (LinearLayout) findViewById(R.id.linear);
-                if (mChart == null) {
+                if (mChart == null){
+                    layout.removeAllViews();
                     initChart();
                     addSampleData();
                     mRenderer.setXTitle("Наблюдения");
@@ -121,9 +163,80 @@ public class MainActivity extends AppCompatActivity {
                     mChart = ChartFactory.getCubeLineChartView(MainActivity.this, mDataset, mRenderer, 0.3f);
                     layout.addView(mChart);
                 } else {
-                    mChart.repaint();
+                    layout.removeAllViews();
+                    //mChart.repaint();
+                    layout.addView(mChart);
                 }
 
+                bt3.setEnabled(true);
+            }
+        });
+
+        bt3 = (Button)findViewById(R.id.button_show_2);
+        bt3.setEnabled(false);
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int n = values10.length;
+                double sum = 0;
+
+                sum = 0;
+                for(int i = 0; i<n;i++){
+                    sum += values4[i]*values4[i]/2;
+                }
+                values[0] = sum/n;
+
+                sum = 0;
+                for(int i = 0; i<n;i++){
+                    sum += values5[i]*values5[i]/2;
+                }
+                values[1] = sum/n;
+
+                sum = 0;
+                for(int i = 0; i<n;i++){
+                    sum += values6[i]*values6[i]/2;
+                }
+                values[2] = sum/n;
+
+                sum = 0;
+                for(int i = 0; i<n;i++){
+                    sum += values7[i]*values7[i]/2;
+                }
+                values[3] = sum/n;
+
+                sum = 0;
+                for(int i = 0; i<n;i++){
+                    sum += values8[i]*values8[i]/2;
+                }
+                values[4] = sum/n;
+
+                sum = 0;
+                for(int i = 0; i<n;i++){
+                    sum += values9[i]*values9[i]/2;
+                }
+                values[5] = sum/n;
+
+                sum = 0;
+                for(int i = 0; i<n;i++){
+                    sum += values10[i]*values10[i]/2;
+                }
+                values[6] = sum/n;
+
+                LinearLayout layout = (LinearLayout) findViewById(R.id.linear);
+                if (mChart2 == null) {
+                    layout.removeAllViews();
+                    initChart2();
+                    addSampleData2();
+                    mRenderer2.setXTitle("Признаки");
+                    mRenderer2.setYTitle("Оценка");
+                    mRenderer2.setXAxisMin(4);
+                    mChart2 = ChartFactory.getCubeLineChartView(MainActivity.this, mDataset2, mRenderer2, 0.3f);
+                    layout.addView(mChart2);
+                } else {
+                    layout.removeAllViews();
+                    //mChart2.repaint();
+                    layout.addView(mChart2);
+                }
             }
         });
 
@@ -149,12 +262,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void addSampleData() {
-        for(int i = 0; i<values1.length; i++) {
-            mCurrentSeries1.add(i,values1[i]);
-            mCurrentSeries2.add(i,values2[i]);
-            mCurrentSeries3.add(i,values3[i]);
+        for(int i = 0; i<values10.length; i++) {
+            mCurrentSeries1.add(i,values10[i]);
+            mCurrentSeries2.add(i,values7[i]);
+            mCurrentSeries3.add(i,values5[i]);
         }
     }
+
+    private void initChart2() {
+        currentSeries = new XYSeries("Оценка");
+        mDataset2.addSeries(currentSeries);
+
+        currentRenderer = new XYSeriesRenderer();
+        currentRenderer.setColor(Color.RED);
+        mRenderer2.addSeriesRenderer(currentRenderer);
+    }
+
+    private void addSampleData2() {
+        for(int i = 0; i<values.length; i++) {
+            currentSeries.add(i+4,values[i]);
+            Log.d(TAG, i+" "+values[i]);
+        }
+    }
+
 
     class RegressAsyncTask extends AsyncTask<Object, Object, String> {
 
@@ -167,6 +297,7 @@ public class MainActivity extends AppCompatActivity {
             String s = "";
             URL url;
             path = "http://192.168.1.103:4444/server/getResult";
+            //path = "http://10.60.0.171:4444/server/getResult";
             Log.d(TAG, path);
             try {
                 url = new URL(path);
@@ -230,10 +361,13 @@ public class MainActivity extends AppCompatActivity {
 
                     res = res.substring(0,res.length()-1);
                     res = res.substring(1);
-                    if(i==0)
-                        val1 = res;
-                    else if(i==1) val2 = res;
-                    else val3 = res;
+                    if(i==0)  val10 = res;
+                    else if(i==1) val9 = res;
+                    else if(i==2) val8 = res;
+                    else if(i==3) val7 = res;
+                    else if(i==4) val6 = res;
+                    else if(i==5) val5 = res;
+                    else val4 = res;
                 }
                 bt2.setEnabled(true);
 
